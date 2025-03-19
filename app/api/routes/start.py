b26006ad5ce6775680535780d29485aee89df781
jmpleo <totrash@duck.com>
2025-03-19 22:28:42 +0300

    quit group start

--- app/api/routes/start.py
+++ app/api/routes/start.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from fastapi import APIRouter, Query, Body, Depends
 from pydantic import BaseModel
 from typing import List
@@ -28,5 +30,6 @@ async def bulk(
     request: PackStart = Body(...),
     task_service: TaskService = Depends(get_task_service)
 ) -> Response:
-    group_id = await task_service.start_group(work.s(n) for n in request.ns)
-    return Response(data={"group_id": group_id})
+    asyncio.create_task(
+        task_service.start_group(work.s(n) for n in request.ns))
+    return Response(data="started")
